aea798cbe6cbda8108ec5c3c42f9c2ef6f2d3d90
Ozy ullah <habibullahozy298796@gmail.com>
2023-09-17 21:19:55 +0600

    updated from habibullahOzy

--- src/Pages/FixedPages/Home/Home.js
+++ src/Pages/FixedPages/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
                     <div class="max-w-md">
                         <h1 class="text-5xl font-bold">Hello there</h1>
                         <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        
+                        <h1>This is selfPortfolio</h1>
                     </div>
                 </div>
             </div>
